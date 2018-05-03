643dc29bf363969181dd8f7d1b3074299fd477b0
Audrey Meadows <s592714@sbuniv.edu>
2018-05-03 21:16:09 +0000

    This program successfully takes in a user defined message, encrypts it using XOR encryption, and decrypts the message.

--- XOREncryption.java
+++ XOREncryption.java
@@ -38,7 +38,7 @@ public class XOREncryption
     {
       binMessageSeg = binMessage.substring(startIndex, endIndex);
 
-       for(int i = 0; i<binMessageSeg.length(); i++)
+      for(int i = 0; i<binMessageSeg.length(); i++)
       {
         if(codebook.charAt(i) == binMessageSeg.charAt(i))
         {
@@ -55,25 +55,6 @@ public class XOREncryption
       endIndex = startIndex + segLength - 1;
       piecesUsed++;
 
-      /*if(message.length() < segLength)
-      {
-        endIndex = message.length() - 1;
-        trigger = true;
-
-      }
-      else
-      {
-        endIndex = segLength - 1;
-      }
-    //System.out.println("out of if");
-
-      binMessageSeg = binaryMessage.substring(startIndex, endIndex);
-      if(trigger != true)
-      {
-        message = message.substring(segLength - 1);
-      }
-      System.out.println(binMessageSeg);
-*/
     }//while
 
     endIndex = binMessage.length() - 1;
@@ -98,5 +79,55 @@ public class XOREncryption
   System.out.println("Binary Encrypted Message: " + binEncryptedMessage);
   System.out.println("Encrypted Message: " + encryptedMessage);
 
+
+  //STEP 8 WHERE binMessage = binEncryptedMessage, binEncryptedMessage = newMessage
+
+    String newMessage = "";
+    startIndex = 0;
+    endIndex  = codebook.length() - 1;
+    String eMessage = new BigInteger(message.getBytes()).toString(2);
+    String binEncryptedMessageSeg = eMessage.substring(startIndex, endIndex);
+
+    while(eMessage.length() - 1 > endIndex)
+    {
+      binEncryptedMessageSeg = eMessage.substring(startIndex, endIndex);
+
+      for(int i = 0; i<binEncryptedMessageSeg.length(); i++)
+      {
+        if(codebook.charAt(i) == binEncryptedMessageSeg.charAt(i))
+        {
+          newMessage = newMessage + '0';
+        }
+        else
+        {
+          newMessage = newMessage + '1';
+        }
+      }//for
+
+      //Update Indexes to move to next section of binEncryptedMessage
+      startIndex = endIndex + 1;
+      endIndex = startIndex + segLength - 1;
+      piecesUsed++;
+
+    }//while
+
+    endIndex = eMessage.length() - 1;
+
+
+
+       for(int i = 0; i<binEncryptedMessageSeg.length(); i++)
+      {
+        if(codebook.charAt(i) == binEncryptedMessageSeg.charAt(i))
+        {
+          newMessage = newMessage + '0';
+        }
+        else
+        {
+          newMessage = newMessage + '1';
+        }
+      }//for
+
+  String decryptedMessage = new String(new BigInteger(eMessage, 2).toByteArray());
+    System.out.print("Decrypted message: " + decryptedMessage);
   }//main
 }//class
